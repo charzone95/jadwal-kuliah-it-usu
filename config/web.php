8e6be0a7d23efe65abfa3c534af378770114b9d3
Charlie Chen <charzone95@gmail.com>
2015-09-09 22:43:06 +0700

    Removed "web" from URL

--- config/web.php
+++ config/web.php
@@ -2,6 +2,12 @@
 
 $params = require(__DIR__ . '/params.php');
 
+
+/** CUSTOM CONFIG **/
+use \yii\web\Request;
+$baseUrl = str_replace('/web', '', (new Request)->getBaseUrl());
+/** END OF CUSTOM CONFIG **/
+
 $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
@@ -15,6 +21,10 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'qTwHYadKRKEMwRWCxgJvFPp0xHf9Ap3_',
+			
+			//custom config
+            'baseUrl' => $baseUrl,
+
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
