24b8ecc209a77d46203c16cb39dd5bb9e191b25e
Charlie Chen <charzone95@gmail.com>
2015-09-09 21:09:05 +0700

    Initial Commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=jadwalkuliahdb',
+    'username' => 'charzone95',
+    'password' => 'charlieganteng',
+    'charset' => 'utf8',
+];
